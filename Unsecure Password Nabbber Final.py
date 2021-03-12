2e2b2522d371830dfb50b8386fb5eb61a254e5d6
HuyinSong24653 <80292613+HuyinSong24653@users.noreply.github.com>
2021-03-12 17:57:49 +1300

    Add files via upload

---
+++ Unsecure Password Nabbber Final.py
@@ -0,0 +1,41 @@
+#Unsecure password nabber.py
+#To log in and store usernames and passwords
+#Huyin Song, 22 February 2021
+
+passwords = ["CoolLiquid312", "BT7274"]
+
+try:
+    def menu(name, age): # A function that takes the user's inputted name and age
+
+        print("Hello, ", name) # Greets the user with their inputted name
+
+        if age < 13: # if condition to check if the user is old enough to access the code
+            print("Sorry, you are not old enough to access this programme") # User is not old enough, is booted off the code
+            exit()
+        else: # User is old enough, the code prints options for the user to select
+            mode = input("""Select a mode with the following numbers-
+1. Add passwords
+2. View passwords
+3. Exit programme
+Please choose a mode : """).strip() # Strips their input of spaces
+            return mode
+
+    print("Welcome to Unsecure Password Nabber") # Welcomes the user to the code
+    name = input("What is your name? : ") # Asks the user for their name
+    age = int(input("What is your age? : ")) # Asks the user for their age
+
+    while True: 
+        chosen_mode = menu(name, age) # Calls the funtion, gives user's name and age
+
+        if chosen_mode == "1": # If the user chose mode 1
+            passwords.append(input("What is the password you wish to add? : ")) # Asks and adds user inputs to passwords list
+        elif chosen_mode == "2": # If the user chose mode 2
+            print(passwords) # Prints the passwords list
+        elif chosen_mode == "3": # If the user chose mode 3
+            break # Lets the user out of the code if they chose mode 3 (Exit the code)
+        else:
+            print("That was incorrect man") # If the user chose something other than the 3 available modes, tells them its incorrect
+
+    print("Thanks for using the programme") # Gives the user a goodbye message for exiting the code from mode 3
+except(ValueError):
+    print("Please input a correct value")
