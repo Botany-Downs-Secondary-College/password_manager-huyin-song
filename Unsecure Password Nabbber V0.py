7e71b14464e2335b04a11f688880e1f8c387c4e7
HuyinSong24653 <80292613+HuyinSong24653@users.noreply.github.com>
2021-03-16 20:39:54 +1300

    Add files via upload

---
+++ Unsecure Password Nabbber V0.py
@@ -0,0 +1,12 @@
+#Unsecure password nabber V0.py
+#To log in and store usernames and passwords
+#Huyin Song, 22 February 2021
+
+def menu(name): # A function that takes the user's inputted name
+
+    print("Hello, ", name) # Greets the user with their inputted name
+
+print("Welcome to Unsecure Password Nabber") # Welcomes the user to the code
+name = input("What is your name? : ") # Asks the user for their name
+
+greeting = menu(name)
